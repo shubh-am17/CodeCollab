3278ce90b39100f43bcd6df8df1118ab723814a0
Shubham Kumar <114120098@nitt.edu>
2023-07-27 00:19:51 +0530

    completed home page and structure of  editor page

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { useNavigate } from "react";
 function EditorPage() {
   return (
-    <>
-    EditorPage
-    </>
-  )
+    <div className="editorpage">
+      <div className="left-panel">
+        <div className="left-panel-inner">
+          <h3>Connected</h3>
+          <div className="user-list">USER 1</div>
+        </div>
+        <div className="left-panel-btns">
+        <button className="btn copyBtn">Copy ROOM ID</button>
+
+        <button className="btn leaveBtn">Leave</button>
+        </div>
+      </div>
+      <div className="right-panel">Here will come editor</div>
+    </div>
+  );
 }
 
-export default EditorPage
+export default EditorPage;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,79 @@
-import React from 'react'
-import '../App.css'
+import React, { useState } from "react";
+import "../App.css";
+import { useNavigate } from 'react-router-dom';
+
 function Home() {
+  const [roomID, setRoomID] = useState("");
+  const [roomCreated, setRoomCreated] = useState(false);
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = Math.floor(Math.random() * 10000000000);
+    console.log(id);
+    setRoomID(id);
+    setRoomCreated(true);
+  };
+  const JoinRoom = () => {
+    // TODO: if room id doesn't exist, show error
+    setRoomID("");
+    setRoomCreated(false);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+    console.log(e.target[1].value);
+    navigate(`/editor/${roomID}`, {
+        state: {
+            username,
+            // to pass data from one route to another
+        },
+    });
+  };
   return (
-    <div className='homepage'>
-        <div className='FormContainer'>
-            <form className='Form'>
-                <input type='text' placeholder='Enter Room ID' className='FormInput'/>
-                <input type='text' placeholder='Enter Your Username' className='FormInput'/>
-                <button className='btn joinBtn'>Join</button>
-                <span>
-                    Don't have an Invite? <a href='/' className='create-room'>Create Room</a>
-                </span>
-            </form>
-        </div>        
+    <div className="homepage">
+      <div className="FormContainer">
+        <form className="Form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            defaultValue={roomID}
+            placeholder="Enter Room ID"
+            className="FormInput"
+            onChange={(e) => setRoomID(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Enter Your Username"
+            className="FormInput"
+            onChange={(e) => setUsername(e.target.value)}
+
+            required
+          />
+          <button type="submit" className="btn joinBtn">{roomCreated ? "Create" : "Join"}
+          </button>
+
+          <span>
+            {!roomCreated ? (
+              <>
+                Do not have an Invite?{" "}
+                <div onClick={e=>createNewRoom(e)} className="create-room">
+                  Create Room
+                </div>
+              </>
+            ) : (
+              <>
+                Already have an Invite?{" "}
+                <div onClick={JoinRoom} className="create-room">
+                  Join Room
+                </div>
+              </>
+            )}
+          </span>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
