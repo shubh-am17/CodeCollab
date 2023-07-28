bb90f795c93c2143c9f70083d565d65119612284
Shubham Kumar <114120098@nitt.edu>
2023-07-29 00:58:22 +0530

    fixed warnings

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Avatar from 'react-avatar';
 
-const Client = ( {username} ) => {
+const Client = ({ username }) => {
     return (
         <div className="client">
             <Avatar name={username} size={50} round="30px" />
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import toast,{Toaster} from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import ACTIONS from "../Actions";
 import Clients from "../components/Clients";
 import Editor from "../components/Editor";
@@ -12,7 +12,6 @@ import {
 } from "react-router-dom";
 
 function EditorPage() {
-
   const socketRef = useRef(null);
   // reference to the WebSocket connection object
   // By using useRef, the socketRef variable retains its value across re-renders. This prevents multiple connection attempts whenever the component re-renders. The WebSocket connection object will be stored in the socketRef.current, and it will be accessible throughout the component's lifetime without the need for reconnecting every time the component updates.
@@ -46,31 +45,34 @@ function EditorPage() {
       });
 
       //Listen for the joined event
-      socketRef.current.on(ACTIONS.JOINED, ({ clients, username, socketId }) => { //emmitted by server.js when it listens to join event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          //emmitted by server.js when it listens to join event
           if (username !== location.state?.username) {
             toast.success(`${username} joined the room.`);
             console.log(`${username} joined`);
           }
           setClients(clients);
           //passing the clients array to the state
-          console.log(clients);
+          // console.log(clients);
+        }
+      );
+
+      // listen for disconnect event
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.error(`${username} left the room.`);
+        setClients((clients) => {
+          return clients.filter((client) => client.socketId !== socketId);
         });
-    
-        // listen for disconnect event
-        socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
-          toast.error(`${username} left the room.`);
-          setClients((clients) => {
-            return clients.filter((client) => client.socketId !== socketId);
-          });
-        });
+      });
     };
     // what if init was called outside useEffect ? = If you call the init function multiple times in your component, the WebSocket connection will be established each time the function is called. This could lead to multiple WebSocket connections, which might not be the intended behavior.
     //
 
-
     init();
 
-    //CLEANING UP 
+    //CLEANING UP
     return () => {
       socketRef.current.disconnect();
       socketRef.current.off(ACTIONS.JOINED);
@@ -79,53 +81,50 @@ function EditorPage() {
   }, []);
   //blank array means it will run only once
 
-  if(!location.state?.username) {
+  if (!location.state?.username) {
     toast.error("Please enter a username.");
-    <Toaster/>
+    <Toaster />;
     return <Navigate to="/" />;
   }
 
-  function handleCopy(){
+  function handleCopy() {
     navigator.clipboard.writeText(roomId);
     toast.success("Copied to clipboard.");
   }
 
-  function handleLeave(){
+  function handleLeave() {
     socketRef.current.disconnect();
     reactNavigator("/");
     //only redirecting user to home will not work as the socket connection is still open
   }
   return (
     <div className="editorpage">
-
       <div className="left-panel">
-      <h1 className="logo">CodeCollab.</h1>
+        <h1 className="logo">CodeCollab.</h1>
 
         <div className="left-panel-inner">
-      <Toaster/>
+          <Toaster />
           <h3>Connected</h3>
           <div className="user-list">
-            {clients.map((client) => {
-              return (
-                <>
-                  <Clients
-                    key={client.socketid}
-                    username={client.username}
-                  ></Clients>
-                </>
-              );
-            })}
+            {clients.map((client) => (
+              <Clients key={client.socketId} username={client.username} />
+            ))}
+
             {/* //TODO: display name of current user as YOU keeping same avatar */}
           </div>
         </div>
         <div className="left-panel-btns">
-          <button className="btn copyBtn" onClick={handleCopy}>Copy ROOM ID</button>
+          <button className="btn copyBtn" onClick={handleCopy}>
+            Copy ROOM ID
+          </button>
 
-          <button className="btn leaveBtn" onClick={handleLeave}>Leave</button>
+          <button className="btn leaveBtn" onClick={handleLeave}>
+            Leave
+          </button>
         </div>
       </div>
       <div className="right-panel">
-        <Editor socketRef={socketRef} roomId={roomId}/>
+        <Editor socketRef={socketRef} roomId={roomId} />
       </div>
     </div>
   );
